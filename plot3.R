74de3fbe911131e9d2e1dee403e341c7b9c14fb0
Cornelius Schumacher <schumacher@kde.org>
2015-03-07 14:44:32 +0100

    Add code and results for plot 3

---
+++ plot3.R
@@ -0,0 +1,21 @@
+columns = c(rep("character", 2), rep("numeric", 7))
+raw_data <- read.table("household_power_consumption.txt", sep = ";",
+                       header = TRUE, colClasses = columns, na.strings = "?")
+
+data <- subset(raw_data, Date == "1/2/2007" | Date == "2/2/2007")
+
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time),
+                            format = "%d/%m/%Y %H:%M:%S")
+
+png("plot3.png")
+
+with(data, plot(DateTime, Sub_metering_1, type="n",
+     ylab = "Energy sub metering"))
+
+with(data, lines(DateTime, Sub_metering_1))
+with(data, lines(DateTime, Sub_metering_2, col = "red"))
+with(data, lines(DateTime, Sub_metering_3, col = "blue"))
+
+legend("topright", colnames(data)[7:9],lwd=(c(1,1)), col=c("black","red","blue"))
+
+dev.off()
